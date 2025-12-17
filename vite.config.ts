07467ddd22505bf89cd24a69712f373448fe8e5d
daveycodez <daveycodez@gmail.com>
2025-12-17 02:28:37 -0800

    Add Vite and vite-plugin-dts, create Vite config
    Added Vite and vite-plugin-dts as dev dependencies in package.json. Introduced a new vite.config.ts file to configure Vite for the project.

---
+++ vite.config.ts
@@ -0,0 +1,29 @@
+import { resolve } from "node:path"
+import { fileURLToPath } from "node:url"
+import { defineConfig } from "vite"
+import dts from "vite-plugin-dts"
+
+const __dirname = fileURLToPath(new URL(".", import.meta.url))
+
+export default defineConfig({
+  plugins: [
+    dts({
+      include: ["src/**/*"]
+    })
+  ],
+  build: {
+    lib: {
+      entry: "src/index.ts",
+      formats: ["es", "cjs"],
+      fileName: "index"
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"]
+    }
+  },
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "src")
+    }
+  }
+})
